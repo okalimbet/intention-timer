da908e5df0bcd52b498c9fb12f00c4d6ad046ea6
Plum <plummer.gv@gmail.com>
2020-09-11 18:54:08 -0600

    create instance activity class, add current activity window, add error message

--- activity.js
+++ activity.js
@@ -1,14 +1,14 @@
 
 class Activity {
-    constructor(category, description, minutes, seconds, completed, id){
+    constructor(category, description, minutes, seconds, completed=false, id=Date.now()){
       this.category = category;
       this.description = description;
       this.minutes = minutes;
       this.seconds = seconds;
       this.completed = completed;
-      this.id = Date.now();
+      this.id = id;
     }
-    
+
     startTimer(){
 
     }
